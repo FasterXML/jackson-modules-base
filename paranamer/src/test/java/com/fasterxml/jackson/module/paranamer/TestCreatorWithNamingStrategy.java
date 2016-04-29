ee379d73b4b8c46271c05afb9ddca33bdf2c05b3
Tatu Saloranta <tatu.saloranta@iki.fi>
2016-04-28 17:33:12 -0700

    move failing test under failing

--- paranamer/src/test/java/com/fasterxml/jackson/module/paranamer/TestCreatorWithNamingStrategy.java
+++ paranamer/src/test/java/com/fasterxml/jackson/module/paranamer/TestCreatorWithNamingStrategy.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 public class TestCreatorWithNamingStrategy
     extends ParanamerTestBase
 {
-
     static class CreatorBean
     {
         protected String myName;
@@ -21,43 +20,13 @@ public class TestCreatorWithNamingStrategy
         }
     }
 
-    static class StaticStringCreatorBean
-    {
-        protected String myName;
-        protected int myAge;
-
-        public StaticStringCreatorBean(int myAge, String myName)
-        {
-            this.myName = myName;
-            this.myAge = myAge;
-        }
-
-        @JsonCreator
-        public static StaticStringCreatorBean parse(String delimited)
-        {
-            String[] args = delimited.split("\\|");
-            if (args.length != 2) {
-                throw new IllegalArgumentException("Invalid string: " + delimited + ". Expected 'age|name'.");
-            }
-            int age = Integer.parseInt(args[0]);
-            return new StaticStringCreatorBean(age, args[1]);
-        }
-    }
-
     private final ObjectMapper MAPPER = new ObjectMapper()
             .registerModule(new ParanamerModule())
-            .setPropertyNamingStrategy(PropertyNamingStrategy.PASCAL_CASE_TO_CAMEL_CASE);
+            .setPropertyNamingStrategy(PropertyNamingStrategy.UPPER_CAMEL_CASE);
 
     public void testSimpleConstructor() throws Exception
     {
         CreatorBean bean = MAPPER.readValue("{ \"MyAge\" : 42,  \"MyName\" : \"NotMyRealName\" }", CreatorBean.class);
-        assertEquals(42, bean.myAge);
-        assertEquals("NotMyRealName", bean.myName);
-    }
-
-    public void testStaticStringCreator() throws Exception
-    {
-        StaticStringCreatorBean bean = MAPPER.readValue("\"42|NotMyRealName\"", StaticStringCreatorBean.class);
         assertEquals(42, bean.myAge);
         assertEquals("NotMyRealName", bean.myName);
     }
