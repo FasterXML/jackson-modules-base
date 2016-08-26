8862f77af4b777877e80e16abd94539f8b9145b3
gil <puntogil@libero.it>
2016-08-26 17:13:03 +0200

    Fix license headers

--- afterburner/src/test/java/com/fasterxml/jackson/module/afterburner/deser/TestPolymorphicCreators.java
+++ afterburner/src/test/java/com/fasterxml/jackson/module/afterburner/deser/TestPolymorphicCreators.java
@@ -1,3 +1,10 @@
+/*
+* Copyright (c) FasterXML, LLC.
+* Licensed to the Apache Software Foundation (ASF)
+* under one or more contributor license agreements;
+* and to You under the Apache License, Version 2.0.
+*/
+
 package com.fasterxml.jackson.module.afterburner.deser;
 
 import com.fasterxml.jackson.annotation.*;
--- mrbean/src/main/java/com/fasterxml/jackson/module/mrbean/package-info.java
+++ mrbean/src/main/java/com/fasterxml/jackson/module/mrbean/package-info.java
@@ -1,3 +1,10 @@
+/*
+* Copyright (c) FasterXML, LLC.
+* Licensed to the Apache Software Foundation (ASF)
+* under one or more contributor license agreements;
+* and to You under the Apache License, Version 2.0.
+*/
+
 /**
  * Package that implements "interface materializer" functionality, whereby
  * abstract classes and interfaces can be used as-is, and framework constructs
