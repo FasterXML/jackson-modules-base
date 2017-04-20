735fa5d3049f71906e5f10c741a2002ca9d59c13
Tatu Saloranta <tatu.saloranta@iki.fi>
2017-04-19 19:56:26 -0700

    Fix some more warnings

--- guice/src/main/java/com/fasterxml/jackson/module/guice/GuiceAnnotationIntrospector.java
+++ guice/src/main/java/com/fasterxml/jackson/module/guice/GuiceAnnotationIntrospector.java
@@ -13,10 +13,24 @@ public class GuiceAnnotationIntrospector extends NopAnnotationIntrospector
 {
     private static final long serialVersionUID = 1L;
 
+    @Override // since 2.9
+    public JacksonInject.Value findInjectableValue(AnnotatedMember m) {
+        Object id = _findGuiceInjectId(m);
+        if (id == null) {
+            return null;
+        }
+        return JacksonInject.Value.forId(id);
+    }
+
+    @Deprecated // since 2.9
     @Override
-    public Object findInjectableValueId(AnnotatedMember m)
+    public Object findInjectableValueId(AnnotatedMember m) {
+        return _findGuiceInjectId(m);
+    }
+
+    private Object _findGuiceInjectId(AnnotatedMember m)
     {
-       /*
+        /*
         * We check on three kinds of annotations: @JacksonInject for types
         * that were actually created for Jackson, and @Inject (both Guice's
         * and javax.inject) for types that (for example) extend already
@@ -24,6 +38,9 @@ public class GuiceAnnotationIntrospector extends NopAnnotationIntrospector
         *
         * Postel's law: http://en.wikipedia.org/wiki/Robustness_principle
         */
+        // 19-Apr-2017, tatu: Actually this is something that should not be done;
+        //   instead, pair of AnnotationIntrospector should be used... Leaving in
+        //   for now, however.
         if ((m.getAnnotation(JacksonInject.class) == null) &&
             (m.getAnnotation(javax.inject.Inject.class) == null) &&
             (m.getAnnotation(com.google.inject.Inject.class) == null))
@@ -34,49 +51,42 @@ public class GuiceAnnotationIntrospector extends NopAnnotationIntrospector
         final AnnotatedMember guiceMember;
         final Annotation guiceAnnotation;
 
-        if ((m instanceof AnnotatedField) || (m instanceof AnnotatedParameter))
-        {
-           /*
-            * On fields and parameters the @Qualifier annotation and type to
+        if ((m instanceof AnnotatedField) || (m instanceof AnnotatedParameter)) {
+           /* On fields and parameters the @Qualifier annotation and type to
             * inject are the member itself, so, nothing to do here...
             */
             guiceMember = m;
             AnnotationMap anns = ((AnnotatedMember) m).getAllAnnotations();
             guiceAnnotation = findBindingAnnotation(anns.annotations());
-        }
-        else if (m instanceof AnnotatedMethod)
-        {
-           /*
-            * For method injection, the @Qualifier and type to inject are
+        } else if (m instanceof AnnotatedMethod) {
+           /* For method injection, the @Qualifier and type to inject are
             * specified on the parameter. Here, we only consider methods with
             * a single parameter.
             */
            final AnnotatedMethod a = (AnnotatedMethod) m;
-           if (a.getParameterCount() != 1) return null;
+           if (a.getParameterCount() != 1) {
+               return null;
+           }
 
-           /*
-            * Jackson does not *YET* give us parameter annotations on methods,
+           /* Jackson does not *YET* give us parameter annotations on methods,
             * only on constructors, henceforth we have to do a bit of work
             * ourselves!
             */
             guiceMember = a.getParameter(0);
             final Annotation[] annotations = a.getMember().getParameterAnnotations()[0];
             guiceAnnotation = findBindingAnnotation(Arrays.asList(annotations));
-        }
-        else
-        {
+        } else {
             /* Ignore constructors */
             return null;
         }
 
-        /*
-         * Depending on whether we have an annotation (or not) return the
+        /* Depending on whether we have an annotation (or not) return the
          * correct Guice key that Jackson will use to query the Injector.
          */
         if (guiceAnnotation == null) {
             // 19-Sep-2016, tatu: Used to pass `getGenericType()`, but that is now deprecated.
             //    Looking at code in Guice Key, I don't think it does particularly good job
-            //    in resolving generic types, so I don't think change
+            //    in resolving generic types, so this is probably safe...
 //            return Key.get(guiceMember.getGenericType());
             return Key.get((java.lang.reflect.Type) guiceMember.getRawType());
         }
