997f31cb608c3dc26513e40f430217a18dfdf6f7
GedMarc <ged_marc@hotmail.com>
2018-11-25 21:14:37 +0200

    Build Fixes, Versions Controlled

--- mrbean/src/main/java/com/fasterxml/jackson/module/mrbean/PackageVersion.java
+++
@@ -1,8 +0,0 @@
-package com.fasterxml.jackson.module.mrbean;
-
-import com.fasterxml.jackson.core.Version;
-
-public class PackageVersion
-{
-	public static Version VERSION = Version.unknownVersion();
-}
