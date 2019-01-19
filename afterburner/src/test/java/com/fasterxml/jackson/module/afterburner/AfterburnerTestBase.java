e3191ad290e4502d50400df8326d8a7b93df71da
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-01-18 18:29:29 -0800

    Fix compilation problems wrt TypeReference/mapper change

--- afterburner/src/test/java/com/fasterxml/jackson/module/afterburner/AfterburnerTestBase.java
+++ afterburner/src/test/java/com/fasterxml/jackson/module/afterburner/AfterburnerTestBase.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 
 public abstract class AfterburnerTestBase extends junit.framework.TestCase
 {
