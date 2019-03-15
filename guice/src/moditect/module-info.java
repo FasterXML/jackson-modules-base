e2165e14caf5d102a3796bd9881482c13c70fb69
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-03-14 21:13:59 -0700

    Start work on getting JDK 9 module info

---
+++ guice/src/moditect/module-info.java
@@ -0,0 +1,13 @@
+// Generated 14-Mar-2019 using Moditect maven plugin
+
+module com.fasterxml.jackson.module.guice {
+    requires javax.inject;
+
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.google.guice;
+
+    exports com.fasterxml.jackson.module.guice;
+
+}
