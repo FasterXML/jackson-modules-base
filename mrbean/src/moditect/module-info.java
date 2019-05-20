8ab22382fcd02b744a94259ae53027a67ed283c4
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-05-19 19:00:11 -0700

    A fix to mrbean module def

--- mrbean/src/moditect/module-info.java
+++ mrbean/src/moditect/module-info.java
@@ -3,6 +3,8 @@
 module com.fasterxml.jackson.module.mrbean {
     requires transitive com.fasterxml.jackson.databind;
 
+    exports com.fasterxml.jackson.module.mrbean;
+
     provides com.fasterxml.jackson.databind.Module with
         com.fasterxml.jackson.module.mrbean.MrBeanModule;
 }
