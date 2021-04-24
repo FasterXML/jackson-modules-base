365426fca72a07e8ccfba1741439f67d3350ff43
Tatu Saloranta <tatu.saloranta@iki.fi>
2021-04-23 19:59:59 -0700

    Re-enable Thread-serialize tests for 2.13 where they pass

--- blackbird/src/test/java/com/fasterxml/jackson/module/blackbird/misc/PreventJDKTypeAccessTest.java
+++ blackbird/src/test/java/com/fasterxml/jackson/module/blackbird/misc/PreventJDKTypeAccessTest.java
@@ -1,19 +1,17 @@
 package com.fasterxml.jackson.module.blackbird.misc;
 
-//import java.util.Map;
+import java.util.Map;
 
-//import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.module.blackbird.BlackbirdTestBase;
 
 public class PreventJDKTypeAccessTest extends BlackbirdTestBase
 {
-//    private final ObjectMapper MAPPER = newObjectMapper();
-//    private final ObjectMapper VANILLA_MAPPER = newVanillaJSONMapper();
+    private final ObjectMapper MAPPER = newObjectMapper();
+    private final ObjectMapper VANILLA_MAPPER = newVanillaJSONMapper();
 
     public void testJDKThreadroundTrip() throws Exception
     {
-        // 23-Apr-2021, tatu: Fixed in 2.13, must comment out for 2.12
-        /*
         final Object input = Thread.currentThread();
         final String json1 = VANILLA_MAPPER.writeValueAsString(input);
         final String json2 = MAPPER.writeValueAsString(input);
@@ -22,6 +20,5 @@ public class PreventJDKTypeAccessTest extends BlackbirdTestBase
         Map<?,?> map2 = MAPPER.readValue(json2, Map.class);
 
         assertEquals(map1.keySet(), map2.keySet());
-    */
     }
 }
