3ff21def50ff3003d84064d8ea6985da27411b95
Tatu Saloranta <tatu.saloranta@iki.fi>
2021-05-13 20:31:34 -0700

    Minor cleanup wrt JAXB/Jakarta separation

--- jaxb/src/moditect/module-info.java
+++ jaxb/src/moditect/module-info.java
@@ -2,12 +2,9 @@ module com.fasterxml.jackson.module.jaxb {
     requires java.logging;
     requires java.xml;
     requires static java.xml.bind;
-    requires static jakarta.xml.bind;
 
     // Needed for JDK9+, but optionally only
     requires static java.activation;
-    // Jakarta Release
-    requires static jakarta.activation;
 
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
