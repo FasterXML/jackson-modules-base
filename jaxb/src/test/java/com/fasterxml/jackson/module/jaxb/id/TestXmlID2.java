a986bde6a757400ebe4de37d54e78c75595f7de8
Ge Yu <yuxx0851@umn.edu>
2021-08-30 15:24:59 -0500

    Restore

--- jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
+++ jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
@@ -1,4 +1,3 @@
-
 package com.fasterxml.jackson.module.jaxb.id;
 
 import java.util.*;
@@ -10,7 +9,6 @@ import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.module.jaxb.BaseJaxbTest;
 import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 public class TestXmlID2 extends BaseJaxbTest
 {
@@ -47,19 +45,15 @@ public class TestXmlID2 extends BaseJaxbTest
     
     @XmlRootElement(name = "user")
     @XmlAccessorType(XmlAccessType.FIELD)
-    //@JsonPropertyOrder({"id","username", "email", "department"})
     static class User
     {
-        @XmlID
-        @XmlElement
+        @XmlElement @XmlID
         public Long id;
-        //@XmlElement(name = "username")
+
         public String username;
-        //@XmlElement(name = "email")
         public String email;
 
         @XmlIDREF
-        //@XmlElement(name = "department")
         public Department department;
 
         protected User() { }
@@ -123,7 +117,6 @@ public class TestXmlID2 extends BaseJaxbTest
                 +"11,{\"id\":22,\"username\":\"22\",\"email\":\"22@test.com\","
                 +"\"department\":9}]}},22,{\"id\":33,\"username\":\"33\",\"email\":\"33@test.com\",\"department\":null}]";
         ObjectMapper mapper = JsonMapper.builder()
-        
         // true -> ignore XmlIDREF annotation
                 .annotationIntrospector(new JaxbAnnotationIntrospector(true))
                 .build();
@@ -142,26 +135,18 @@ public class TestXmlID2 extends BaseJaxbTest
     
     public void testIdWithJaxbRules() throws Exception
     {
-        //ObjectMapper mapper = new ObjectMapper();
         ObjectMapper mapper = JsonMapper.builder()
         // but then also variant where ID is ALWAYS used for XmlID / XmlIDREF
                 .annotationIntrospector(new JaxbAnnotationIntrospector())
                 .build();
         List<User> users = getUserList();
         final String json = mapper.writeValueAsString(users);
-
         String expected = "[{\"id\":11,\"username\":\"11\",\"email\":\"11@test.com\",\"department\":9}"
                 +",{\"id\":22,\"username\":\"22\",\"email\":\"22@test.com\",\"department\":9}"
                 +",{\"id\":33,\"username\":\"33\",\"email\":\"33@test.com\",\"department\":null}]";
-
+        
         assertEquals(expected, json);
 
         // However, there is no way to resolve those back, without some external mechanism...
     }
 }
-// //expected
-// [{"id":11,"[username":"11","email":"11@test.com","department":9},
-//  {"id":22,"username":"22","email":"22@test.com","department":9},
-//  {"id":33,"username":"33","email":"33@test.com","department":null]}]
-
-// but was:<[{"id":11,"[department":{"id":9,"employees":[11,{"id":22,"department":9,"username":"22","email":"22@test.com"}],"name":"department9"},"username":"11","email":"11@test.com"},22,{"id":33,"department":null,"username":"33","email":"33@test.com"]}]>
