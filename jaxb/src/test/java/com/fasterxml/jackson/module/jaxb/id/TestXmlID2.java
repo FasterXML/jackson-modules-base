417aacef07e05fbe2853c776d7f8a309583b9966
alany9552 <gey2@illinois.edu>
2021-08-29 21:55:37 -0500

    asd

--- jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
+++ jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
@@ -44,17 +44,17 @@ public class TestXmlID2 extends BaseJaxbTest
     }
     
     
-    //@XmlRootElement(name = "user")
-    // @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlRootElement(name = "user")
+    @XmlAccessorType(XmlAccessType.FIELD)
     @JsonPropertyOrder({"id","username", "email", "department"})
     static class User
     {
-        //@XmlElement @XmlID
+        @XmlElement @XmlID
         public Long id;
         public String username;
         public String email;
 
-        //@XmlIDREF
+        @XmlIDREF
         public Department department;
 
         protected User() { }
