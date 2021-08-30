12cd5e9beaf76dd0cae81f0bf637d6830f2e661b
alany9552 <gey2@illinois.edu>
2021-08-29 20:08:07 -0500

    ?

--- jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
+++ jaxb/src/test/java/com/fasterxml/jackson/module/jaxb/id/TestXmlID2.java
@@ -142,6 +142,7 @@ public class TestXmlID2 extends BaseJaxbTest
         // but then also variant where ID is ALWAYS used for XmlID / XmlIDREF
                 .annotationIntrospector(new JaxbAnnotationIntrospector())
                 .build();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
         List<User> users = getUserList();
         // System.out.println("XXXXXXXXXXXXXXXXXHHHHHHHHHHHHHHHHHHHXXXXXXXXXXX");
         // for(int i = 0; i < users.length(); i ++) {
