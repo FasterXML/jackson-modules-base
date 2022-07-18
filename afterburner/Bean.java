3079606594d7140ca92d393ef627fc069c10d66e
Tatu Saloranta <tatu.saloranta@iki.fi>
2022-07-17 20:48:24 -0700

    Try to fix "IsolatedClassLoaderTest" failure

---
+++ afterburner/Bean.java
@@ -0,0 +1,5 @@
+// 17-Jul-2022, tatu: Source for `Bean.class` used by `IsolatedClassLoaderTest`
+public class Bean {
+  private String value = "some string";
+  public String getValue() { return value; }
+}
