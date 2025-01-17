7ce5e114b9bd489d3096daae017b9052bed8fb60
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-01-16 20:32:30 -0800

    Convert Guice7 module

---
+++ guice7/src/test/java/module-info.java
@@ -0,0 +1,22 @@
+// Guice7-module (unit) Test Module descriptor
+module tools.jackson.module.guice7
+{
+    // Since we are not split from Main artifact, will not
+    // need to depend on Main artifact -- but need its dependencies
+
+    requires com.fasterxml.jackson.annotation;
+    requires tools.jackson.core;
+    requires tools.jackson.databind;
+
+    requires com.google.guice;
+
+    //Jakarta Reference Implementation
+    requires static jakarta.inject;
+
+    // Additional test lib/framework dependencies
+
+    requires junit; // JUnit4 To Be Removed in future
+
+    // Further, need to open up some packages for JUnit et al
+    opens tools.jackson.module.guice7;
+}
