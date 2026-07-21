9bb2cdc1b6517414e42825cd5c2db70a942029f1
Tatu Saloranta <tatu.saloranta@iki.fi>
2026-07-20 19:37:04 -0700

    Tweaking, remove actual code check leave tests

--- blackbird/src/test/java/tools/jackson/module/blackbird/deser/OptionalDeser355Test.java
+++ blackbird/src/test/java/tools/jackson/module/blackbird/deser/OptionalDeser355Test.java
@@ -24,7 +24,7 @@ import tools.jackson.module.blackbird.BlackbirdTestBase;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class OptionalDeserTest extends BlackbirdTestBase
+public class OptionalDeser355Test extends BlackbirdTestBase
 {
     static class OptionalBean {
         private Optional<String> value;
@@ -40,6 +40,7 @@ public class OptionalDeserTest extends BlackbirdTestBase
 
     static class CustomOptionalBean {
         private Optional<String> value;
+        private Optional<Integer> number;
 
         public Optional<String> getValue() {
             return value;
@@ -49,11 +50,28 @@ public class OptionalDeserTest extends BlackbirdTestBase
         public void setValue(Optional<String> value) {
             this.value = value;
         }
+
+        public Optional<Integer> getNumber() {
+            return number;
+        }
+
+        @JsonDeserialize(using = CustomOptionalDeserializer.class)
+        public void setNumber(Optional<Integer> number) {
+            this.number = number;
+        }
     }
 
     static class CustomOptionalDeserializer extends Jdk8OptionalDeserializer {
+        /**
+         * Placeholder used by the no-arguments constructor that
+         * {@code @JsonDeserialize(using=...)} calls: the actual declared type is
+         * only known once {@link #createContextual} gets called.
+         */
+        private static final JavaType UNRESOLVED_TYPE = TypeFactory.createDefaultInstance()
+                .constructType(Optional.class);
+
         public CustomOptionalDeserializer() {
-            this(optionalStringType(), null, null);
+            this(UNRESOLVED_TYPE, null, null);
         }
 
         private CustomOptionalDeserializer(JavaType fullType,
@@ -62,15 +80,22 @@ public class OptionalDeserTest extends BlackbirdTestBase
         }
 
         @Override
+        public ValueDeserializer<?> createContextual(DeserializationContext ctxt,
+                BeanProperty property) {
+            // Resolve against whatever `Optional<T>` the property actually declares,
+            // instead of assuming one specific value type
+            JavaType declaredType = (property == null) ? _fullType : property.getType();
+            if (!declaredType.equals(_fullType)) {
+                return new CustomOptionalDeserializer(declaredType, null, null)
+                        .createContextual(ctxt, property);
+            }
+            return super.createContextual(ctxt, property);
+        }
+
+        @Override
         public CustomOptionalDeserializer withResolved(TypeDeserializer typeDeser,
                 ValueDeserializer<?> valueDeser) {
             return new CustomOptionalDeserializer(_fullType, typeDeser, valueDeser);
-        }
-
-        private static JavaType optionalStringType() {
-            TypeFactory typeFactory = TypeFactory.createDefaultInstance();
-            return typeFactory.constructReferenceType(Optional.class,
-                    typeFactory.constructType(String.class));
         }
     }
 
@@ -103,7 +128,7 @@ public class OptionalDeserTest extends BlackbirdTestBase
     }
 
     @Test
-    public void keepsOptimizedPropertyForBuiltInOptionalDeserializer() throws Exception {
+    public void testKeepsOptimizedPropertyForBuiltInOptionalDeserializer() throws Exception {
         DeserializerCapture capture = new DeserializerCapture();
         ObjectMapper mapper = mapperWithCapture(capture);
 
@@ -115,16 +140,19 @@ public class OptionalDeserTest extends BlackbirdTestBase
     }
 
     @Test
-    public void fallsBackForCustomOptionalDeserializer() throws Exception {
+    public void testFallsBackForCustomOptionalDeserializer() throws Exception {
         DeserializerCapture capture = new DeserializerCapture();
         ObjectMapper mapper = mapperWithCapture(capture);
 
         CustomOptionalBean bean = mapper.readValue(
-                "{\"value\":\"test\"}", CustomOptionalBean.class);
+                "{\"value\":\"test\", \"number\":42}", CustomOptionalBean.class);
 
         assertEquals(Optional.of("test"), bean.getValue());
+        assertEquals(Optional.of(42), bean.getNumber());
         assertInstanceOf(MethodProperty.class,
                 capture.propertyFor(CustomOptionalBean.class, "value"));
+        assertInstanceOf(MethodProperty.class,
+                capture.propertyFor(CustomOptionalBean.class, "number"));
     }
 
     private ObjectMapper mapperWithCapture(DeserializerCapture capture) {
